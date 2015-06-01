bf65dce01e161a36fef1787871e11c0633176ff4
Daniel Harris <bobth3bum@gmail.com>
2015-06-01 16:02:24 -0600

    add 1000 per day limit check for API

--- app/controllers/api/v1/serve_controller.rb
+++ app/controllers/api/v1/serve_controller.rb
@@ -3,8 +3,12 @@ class Api::V1::ServeController < ApplicationController
   def all
     @user = User.find_by_api_key(params[:api_key])
     if @user
-      @user.increment!(:api_requests)
-      render json: FoodTruck.all
+      if @user.api_request > 1000
+        render json: {:error => "You have exceeded your 1000 requests per day limit."}
+      else
+        @user.increment!(:api_requests)
+        render json: FoodTruck.all
+      end
     else
       render json: {:error => "Incorrect or unregistered API Key request."}
     end
