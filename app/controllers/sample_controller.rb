e1191a7e1000b087d325e382e0d8a129f7f0c8ee
Daniel Harris <bobth3bum@gmail.com>
2015-06-01 15:03:39 -0600

    change variable name

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -5,12 +5,11 @@ class SampleController < ApplicationController
   end
 
   def sample
-    # request = request.env["HTTP_REFERER"]
-    # if request == "http://foodtruckin.herokuapp.com/"
-    #   render json: FoodTruck.all
-    # else
-    #   render json: { :error => "Use /api/v1/all for all api requests."}
-    # end
-    render json: FoodTruck.all
+    url = request.env["HTTP_REFERER"]
+    if url == "http://foodtruckin.herokuapp.com/"
+      render json: FoodTruck.all
+    else
+      render json: { :error => "Use /api/v1/all for all api requests."}
+    end
   end
 end
